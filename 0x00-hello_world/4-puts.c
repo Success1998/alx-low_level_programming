4d00ecb437e729a744fbece0d7754c901b2ff958
success1998 <raphealsuccess3@gmail.com>
2022-12-14 02:57:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- *  main - A program that prints a line with puts function
- *  Return: 0 (Success)
+ * main - Entry point
+ * Return: Always return 0 (Success)
  */
+
 int main(void)
 {
 puts("/"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
