efd121bb8176d884dba267b9bb6357c0cea2c500
success1998 <raphealsuccess3@gmail.com>
2022-12-20 10:42:49 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,14 +11,16 @@ void puts_half(char *str)
 
 	longi = 0;
 
-	for (a = 0; str[a] != '\0'; a++)
-		longi++;
+for (a = 0; str[a] != '\0'; a++)
+	longi++;
 
-	n = (longi / 2);
+n = (longi / 2);
 
-	f ((longi % 2) == 1)
-		n = ((longi + 1) / 2);
-	for (a = n; str[a] != '\0'; a++)
+if ((longi % 2) == 1)
+	n = ((longi + 1) / 2);
+
+for (a = n; str[a] != '\0'; a++)
 	_putchar(str[a]);
-	_putchar('\n');
+_putchar('\n');
 }
+
